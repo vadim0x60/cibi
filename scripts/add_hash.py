45196e3ea8f92194d06e9c57a4c8d1be0daca298
Vadim Liventsev <vadim@0x60.ru>
2020-06-18 22:32:51 +0300

    Experiment hashing (bad sketch)

---
+++ scripts/add_hash.py
@@ -0,0 +1,38 @@
+import click
+import os
+import yaml
+
+def hash_dict(d):
+    if type(d) == dict:
+        return hash((k, hash_dict(v)) for k, v in d.items())
+    else:
+        return hash(d)
+
+@click.command()
+@click.argument('exp_dir')
+def add_hash_cmd(exp_dir):
+    add_hash(exp_dir)
+
+def add_hash(exp_dir):
+    if not os.path.isdir(exp_dir):
+        return
+
+    for path in os.listdir(exp_dir):
+        add_hash(os.path.join(exp_dir, path))
+
+    summary_path = os.path.join(exp_dir, 'summary.yml')
+    experiment_path = os.path.join(exp_dir, 'experiment.yml')
+
+    if os.path.exists(summary_path) and os.path.exists(experiment_path):
+        with open(summary_path, 'r') as summary_f:
+            summary = yaml.load(summary_f)
+
+        with open(experiment_path, 'r') as experiment_f:
+            experiment = yaml.load(experiment_f)
+
+        with open(summary_path, 'w') as summary_f:
+            summary['experiment'] = hash_dict(experiment)
+            yaml.dump(summary, summary_f)
+
+if __name__ == '__main__':
+    add_hash_cmd()
