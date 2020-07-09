3e26fb97783a30dd74c506baacc6843504efb06a
Vadim Liventsev <vadim@0x60.ru>
2020-07-10 01:11:47 +0300

    I am _very_ particular

--- scripts/status.py
+++ scripts/status.py
@@ -4,19 +4,26 @@ import os
 @click.command()
 @click.argument('exp_dir')
 def status_cmd(exp_dir):
-    status(exp_dir)
+    exp_dir = os.path.split(os.path.realpath(exp_dir))
+    status(os.path.join(*exp_dir[:-1]), exp_dir[-1])
 
-def status(exp_dir):
+def status(parent_dir, exp_name):
+    exp_dir = os.path.join(parent_dir, exp_name)
     if not os.path.isdir(exp_dir):
         return
 
-    for path in os.listdir(exp_dir):
-        status(os.path.join(exp_dir, path))
+    subdirs = list(os.listdir(exp_dir))
+    try:
+        subdirs = sorted(subdirs, key=lambda x: int(x))
+    except ValueError:
+        pass
+
+    for subdir in subdirs:
+        status(exp_dir, subdir)
 
     summary_path = os.path.join(exp_dir, 'summary.yml')
     experiment_path = os.path.join(exp_dir, 'experiment.yml')
     top_path = os.path.join(exp_dir, 'top.pickle')
-    exp_name = os.path.split(exp_dir)[-1]
 
     if os.path.exists(experiment_path):
         if os.path.exists(summary_path):
