c8bd654c45c3b38e6185c1b514032c8e8cef6010
Vadim Liventsev <hi@vadim.me>
2020-10-16 07:33:06 +0200

    Fixed outdated scripts

--- scripts/duplicate_experiments.py
+++ scripts/duplicate_experiments.py
@@ -15,14 +15,14 @@ def parse_ranges(ranges):
                 yield idx
 
 @click.command()
+@click.argument('exp_dir', type=str)
 @click.argument('ids', type=str)
-def duplicate_experiments(ids):
-    experiments_dir = get_project_dir('experiments')
-    max_experiment_id = max(*(int(idx) for idx in os.listdir(experiments_dir)))
+def duplicate_experiments(exp_dir, ids):
+    max_experiment_id = max(*(int(idx) for idx in os.listdir(exp_dir)))
     for idx in parse_ranges(ids):
         max_experiment_id += 1
-        shutil.copytree(os.path.join(experiments_dir, str(idx)),
-                        os.path.join(experiments_dir, str(max_experiment_id)))
+        shutil.copytree(os.path.join(exp_dir, str(idx)),
+                        os.path.join(exp_dir, str(max_experiment_id)))
 
 if __name__ == '__main__':
     duplicate_experiments()
