3034e0fdeda746480c53bb68b04e8629c62cf401
Vadim Liventsev <hi@vadim.me>
2020-11-05 19:02:04 +0100

    Taxi program 2 with collision avoidance

--- scripts/compile.py
+++ scripts/compile.py
@@ -5,15 +5,28 @@ import click
 def compile(dev_file):
     assert dev_file[-8:] == '_dev.txt'
 
-    code = ''
-
     with open(dev_file, 'r') as dev_f:
-        for line in dev_f.readlines():  
-            if line and line[0] != '#':
+        with open(dev_file[:-8] + '.txt', 'w') as out_f:
+            code = ''
+            def new_program():
+                nonlocal code
+                if code:
+                    out_f.write(code + '\n')
+                code = ''
+
+            new_program()
+
+            for line in dev_f.readlines():
+                if not line or line[0] == '#':
+                    continue
+
+                if line[:3] == '===':
+                    new_program()
+                    continue
+
                 code += line.strip()
-                    
-    with open(dev_file[:-8] + '.txt', 'w') as out_f:
-        out_f.write(code)
+            
+            new_program()
 
 if __name__ == '__main__':
     compile()
