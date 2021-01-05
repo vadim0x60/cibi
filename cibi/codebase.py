5f564469130f6ab65f35dd9db36c1890fa613957
Vadim Liventsev <hi@vadim.me>
2021-01-05 12:46:14 +0300

    Fail faster

--- cibi/codebase.py
+++ cibi/codebase.py
@@ -40,12 +40,11 @@ class Codebase():
         columns = ['code', 'count'] + metrics + metadata
         types = [object, int] + [float for m in metrics] + [object for m in metadata]
 
-        try:
-            assert self.save_file
+        if self.save_file:
             self.data_frame = pd.read_pickle(save_file)
             assert list(self.data_frame.columns) == columns
             assert list(self.data_frame.dtypes) == types
-        except (FileNotFoundError, AssertionError):
+        else:
             self.data_frame = make_dataframe(columns=columns, 
                                              dtypes=types, 
                                              index_column='code' if deduplication else None)
