c467bdba50bc2e23631eb4dd4c5adfe8fb1062d3
Vadim Liventsev <hi@vadim.me>
2021-01-05 11:58:47 +0300

    Rewrote finalizer

--- cibi/train.py
+++ cibi/train.py
@@ -41,54 +41,23 @@ def make_seed_codebase(seed_file, env, observation_discretizer, action_sampler):
 
     return seed_codebase
 
-@click.command()
-@click.argument('logdir', type=str)
-def run_experiments(logdir):
-    with open(os.path.join(logdir, 'experiment.yml'), 'r') as f:
-        config = yaml.safe_load(f)
-        config_hash = calc_hash(config)
+def expand_seed_path(logdir, seed_path):
+    if not seed_path:
+        return None
 
-    required_version = str(config['cibi-version'])
-    assert cibi.__version__.startswith(required_version)
-
-    render = config.get('render', False)
-    discretization_config = config.get('discretization', {})
-
-    scrum_keys = ['cycle-programs', 'syntax-error-reward', 'replay-temperature']
-    scrum_config = {key.replace('-', '_'): config[key] for key in scrum_keys if key in config}
-
-    team = teams[config['team']]
-    env = make_gym(config['env'])
-
-    max_failed_sprints = config.get('max-failed-sprints', 10)
-    max_sprints = config.get('max-sprints', 1000000) * len(team)
-    max_sprints_without_improvement = config.get('max-sprints-without-improvement', 1000000) * len(team)
-
-    early_stopping = EVEEarlyStopping(mode='max', patience=max_sprints_without_improvement*len(team))
+    hardcoded_path = os.path.join(get_project_dir('codebases'), seed_path)
+    custom_path = os.path.join(logdir, seed_path)
     
-    os.makedirs(logdir, exist_ok=True)
+    if os.path.isfile(hardcoded_path):
+        return hardcoded_path
+    elif os.path.isfile(custom_path):
+        return custom_path
+    else:
+        err = f"Seed codebase {seed_path} not found"
+        logger.error(err)
+        raise FileNotFoundError(err)
 
-    scrum_config['program_file'] = os.path.join(logdir, 'programs.pickle')
-
-    logger.setLevel(config.get('log_level', 'INFO'))
-    logger.addHandler(logging.FileHandler(f'{logdir}/log.log'))
-    logger.info(config['env'])
-    start_time = time.monotonic()
-
-    seed = config.get('seed')
-    if seed:
-        hardcoded_path = os.path.join(get_project_dir('codebases'), config['seed'])
-        custom_path = os.path.join(logdir, config['seed'])
-        
-        if os.path.isfile(hardcoded_path):
-            seed = hardcoded_path
-        elif os.path.isfile(custom_path):
-            seed = custom_path
-        else:
-            err = f"Seed codebase {config['seed']} not found"
-            logger.error(err)
-            raise FileNotFoundError(err)
-
+def load_summary(logdir, config_hash):
     try:
         with open(os.path.join(logdir, 'summary.yml'), 'r') as f:
             # This is an attempt to continue a previously trusted experiment
@@ -112,11 +81,50 @@ def run_experiments(logdir):
             'max-total-reward': float('-inf'),
             'experiment': config_hash
         }
+
+    return summary
+
+def run_experiments(logdir, finalize_now):
+    # PREINIT - Reading the experiment's config file
+    with open(os.path.join(logdir, 'experiment.yml'), 'r') as f:
+        config = yaml.safe_load(f)
+        config_hash = calc_hash(config)
+
+    logger.setLevel(config.get('log_level', 'INFO'))
+    logger.addHandler(logging.FileHandler(f'{logdir}/log.log'))
+    logger.info(config['env'])
+
+    logger.info('INIT - Understanding experiment configuration and current state')
+    # (the experiment can be halfway done)
+
+    required_version = str(config['cibi-version'])
+    assert cibi.__version__.startswith(required_version)
+
+    render = config.get('render', False)
+    discretization_config = config.get('discretization', {})
+
+    scrum_keys = ['cycle-programs', 'syntax-error-reward', 'replay-temperature']
+    scrum_config = {key.replace('-', '_'): config[key] for key in scrum_keys if key in config}
+
+    team = teams[config['team']]
+    env = make_gym(config['env'])
+
+    max_failed_sprints = config.get('max-failed-sprints', 10)
+    max_sprints = config.get('max-sprints', 1000000) * len(team)
+    max_sprints_without_improvement = config.get('max-sprints-without-improvement', 1000000) * len(team)
+
+    early_stopping = EVEEarlyStopping(mode='max', patience=max_sprints_without_improvement*len(team))
+
+    scrum_config['program_file'] = os.path.join(logdir, 'programs.pickle')
+
+    seed = config.get('seed')
+    seed = expand_seed_path(logdir, seed)
+
+    summary = load_summary(logdir, config_hash)
     
     summary['cibi-version'] = cibi.__version__
     scrum_config['sprints_elapsed'] = summary['sprints-elapsed']
     scrum_config['quality_callback'] = early_stopping.register
-    start_time -= summary['seconds-elapsed']
 
     train_dir = os.path.join(logdir, 'train')
     events_dir = os.path.join(logdir, 'events')
@@ -129,58 +137,82 @@ def run_experiments(logdir):
 
     random_agent = bf.Executable('@!', observation_discretizer, action_sampler, cycle=True, debug=False)
     bf_io.burn_in(env, random_agent, observation_discretizer, action_sampler)
-    seed_codebase = make_seed_codebase(seed, env, observation_discretizer, action_sampler)
 
-    failed_sprints = 0
-    with hire_team(team, env, observation_discretizer, action_sampler, language,
-                   train_dir, events_dir, scrum_config, seed_codebase) as agent:
-        max_episode_length = config.get('max-episode-length')
+    agent = None
 
-        while (agent.sprints_elapsed < max_sprints and early_stopping.proceed):
-            try:
-                rollout = agent.attend_gym(env, max_reps=max_episode_length, render=render)
+    if not finalize_now:
+        logger.info('TRAIN - Running many iterations of instant scrum')
 
-                episode_length = len(rollout)
-                summary['shortest-episode'] = min(summary['shortest-episode'], episode_length)
-                summary['longest-episode'] = max(summary['longest-episode'], episode_length)
-                if summary['max-total-reward'] < rollout.total_reward:
-                    summary['max-total-reward'] = float(rollout.total_reward)
-                    
-                summary['sprints-elapsed'] = agent.sprints_elapsed
-                summary['seconds-elapsed'] = time.monotonic() - start_time
+        start_time = time.monotonic()
+        start_time -= summary['seconds-elapsed']
 
-                with open(os.path.join(logdir, 'summary.yml'), 'w') as f:
-                    yaml.dump(summary, f)
+        seed_codebase = make_seed_codebase(seed, env, observation_discretizer, action_sampler)
 
-                failed_sprints = 0
-            except KeyboardInterrupt:
-                logger.info('Keyboard interrupt received. Winding down')
-                break
-            except Exception as e:
-                logger.error(traceback.format_exc())
-                failed_sprints += 1
-                if failed_sprints > max_failed_sprints:
-                    logger.error('Tolerance for failed sprints exceeded')
-                    raise e
+        failed_sprints = 0
+        with hire_team(team, env, observation_discretizer, action_sampler, language,
+                    train_dir, events_dir, scrum_config, seed_codebase) as agent:
+            max_episode_length = config.get('max-episode-length')
 
-        top_candidates = agent.archive_branch.top_k('test_quality', 256)
-        ensure_enough_test_runs(top_candidates, env, observation_discretizer, action_sampler)
-        top_program, top_metrics, top_metadata = top_candidates.top_k('test_quality', 1).peek()
+            while (agent.sprints_elapsed < max_sprints and early_stopping.proceed):
+                try:
+                    rollout = agent.attend_gym(env, max_reps=max_episode_length, render=render)
 
-        summary['top'] = {
-            'code': str(top_program),
-            'author': str(top_metadata['author']),
-            'method': str(top_metadata['method']),
-            'parent1': str(top_metadata['parent1']),
-            'parent2': str(top_metadata['parent2']),
-            'test_quality': float(top_metrics['test_quality'])
-        }
+                    episode_length = len(rollout)
+                    summary['shortest-episode'] = min(summary['shortest-episode'], episode_length)
+                    summary['longest-episode'] = max(summary['longest-episode'], episode_length)
+                    if summary['max-total-reward'] < rollout.total_reward:
+                        summary['max-total-reward'] = float(rollout.total_reward)
+                        
+                    summary['sprints-elapsed'] = agent.sprints_elapsed
+                    summary['seconds-elapsed'] = time.monotonic() - start_time
 
-        top_candidates.data_frame.to_pickle(os.path.join(logdir, 'top.pickle'))
+                    with open(os.path.join(logdir, 'summary.yml'), 'w') as f:
+                        yaml.dump(summary, f)
 
-        logger.info(f'Summary: {summary}')
-        with open(os.path.join(logdir, 'summary.yml'), 'w') as f:
-            yaml.dump(summary, f)
+                    failed_sprints = 0
+                except KeyboardInterrupt:
+                    logger.info('Keyboard interrupt received. Winding down')
+                    break
+                except Exception as e:
+                    logger.error(traceback.format_exc())
+                    failed_sprints += 1
+                    if failed_sprints > max_failed_sprints:
+                        logger.error('Tolerance for failed sprints exceeded')
+                        raise e
+
+    logger.info('FINALIZE - Thoroughly testing the 100 best programs found and saving the final score')
+
+    if agent:
+        archive_branch = agent.archive_branch
+    else:
+        archive_branch = make_prod_codebase(deduplication=True, 
+                                            save_file=scrum_config['program_file'])
+        assert len(archive_branch), 'Trying to finalize training with no programs written'
+
+    top_candidates = archive_branch.top_k('test_quality', 256)
+    ensure_enough_test_runs(top_candidates, env, observation_discretizer, action_sampler)
+    top_program, top_metrics, top_metadata = top_candidates.top_k('test_quality', 1).peek()
+
+    summary['top'] = {
+        'code': str(top_program),
+        'author': str(top_metadata['author']),
+        'method': str(top_metadata['method']),
+        'parent1': str(top_metadata['parent1']),
+        'parent2': str(top_metadata['parent2']),
+        'test_quality': float(top_metrics['test_quality'])
+    }
+
+    top_candidates.data_frame.to_pickle(os.path.join(logdir, 'top.pickle'))
+
+    logger.info(f'Summary: {summary}')
+    with open(os.path.join(logdir, 'summary.yml'), 'w') as f:
+        yaml.dump(summary, f)
+
+@click.command()
+@click.argument('logdir', type=str)
+@click.option('--finalize-now', is_flag=True)
+def run_experiments_cmd(**kwargs):
+    return run_experiments(**kwargs)
 
 if __name__ == '__main__':
-    run_experiments()
+    run_experiments_cmd()
