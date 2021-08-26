b29e87d09010cbd2a7de8648c62880a413eafdd0
Vadim Liventsev <hi@vadim.me>
2021-08-26 02:40:31 +0200

    Fixed jun test

--- cibi_test/genetic.py
+++ cibi_test/genetic.py
@@ -14,18 +14,18 @@ language = bf.BFLanguage()
 
 dev = JuniorDeveloper()
 dev = dev.hire(language)
-codebase = Codebase(metrics=['total_reward'])
+codebase = Codebase(metrics=['quality'])
 for program in [
     "+.>4ae.",
     "+.e.4e",
     "2.c-[4c,][4][]+>e3-",
     "c1,>[d,+..2]e<"
 ]:
-    codebase.commit(program, metrics={'total_reward': 1})
+    codebase.commit(program, metrics={'quality': 1})
 
 for gen in range(10):
     dev_branch = dev.write_programs(codebase)
     print(dev_branch['code'])
 
     for code in dev_branch['code']:
-        codebase.commit(code, metrics={'total_reward': 1})
+        codebase.commit(code, metrics={'quality': 1})
